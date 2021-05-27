0380e9c3441d0cc657c36e91b3020723ba03f667
AM KK <andrzej.kapczynski@op.pl>
2021-05-27 19:12:55 +0200

    Update main_file.cpp

--- main_file.cpp
+++ main_file.cpp
@@ -20,6 +20,7 @@ float aspectRatio = 1;
 float camera_z = -20.0f;
 float camera_y = 10.0f; //pozycja kamery
 //KOMENTARZtthttyt
+//proba 2
 
 //Procedura obsługi błędów
 void error_callback(int error, const char* description) {
